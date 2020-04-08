529019e89e667f25c864c26f575e04d3b81a17d2
Kevin Carpio <kevincarpio135@gmail.com>
2020-04-07 17:55:34 -0700

    updated code

--- PodRace.java
+++ PodRace.java
@@ -4,6 +4,27 @@ import java.util.HashMap;
 
 public class PodRace {
     public static Set<Pod> race(double distance, Set<Pod> racers, double timeSlice, double timeLimit) {
-        // IMPLEMENT THIS METHOD
+        if (distance <= 0) {
+            throw new IllegalArgumentException("Distance cannot be less than 0");
+        }
+        if (timeLimit < 0 ){
+          throw new IllegalArgumentException("The time cannot be a negative");
+        }
+
+        var distances = new HashMap<Pod, Double>();
+
+        var winners = new HashSet<Pod>();
+        for (var t = 0.0; t < timeLimit; t += timeSlice) {
+            for (var pod : racers) {
+                var distanceForThisSlice = pod.distanceTraveled(0.0, timeLimit, timeSlice );
+                distances.put(pod,distances.getOrDefault(pod, 0.0) + distanceForThisSlice);
+                if (distances.get(pod) >= distance) {
+                    winners.add(pod);
+                }
+                
+            }
+            break;
+        }
+        return winners;
     }
 }
